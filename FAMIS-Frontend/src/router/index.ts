245e29ced5d404319c475f94928d514797c872f0
Bambam <bambam.pataranan@gmail.com>
2025-05-25 23:53:39 +0700

    Create 'uploadFile' and 'previewFile' index

--- FAMIS-Frontend/src/router/index.ts
+++ FAMIS-Frontend/src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import UploadFile from '@/views/UploadFileView.vue'
+import PreviewFile from '@/views/PreviewFileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'uploadFile',
       component: UploadFile,
+    },
+    {
+      path: '/previewFile',
+      name: 'previewFile',
+      component: PreviewFile,
     }
   ],
 })
