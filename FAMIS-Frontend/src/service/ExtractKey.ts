01bcc66a4518d4a7037d883974ab85a0f16ab922
Bambam <bambam.pataranan@gmail.com>
2025-05-29 19:22:42 +0700

    Add API client to upload file for processing
    "Extract key" service

---
+++ FAMIS-Frontend/src/service/ExtractKey.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'http://127.0.0.1:5001',  
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    
+  }
+});
+
+export default {
+  processFile(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return apiClient.post('/process', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'  
+      }
+    });
+  }
+};
